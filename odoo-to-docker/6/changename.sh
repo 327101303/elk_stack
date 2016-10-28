07c7b5dd8176727b3a373a824e803bbbdc528a08
jun zhang <327101303@qq.com>
2016-10-28 10:34:40 +0800

    no message

---
+++ odoo-to-docker/6/changename.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+file=`date +%Y%m%d-%H%M%S`
+Sfile=/opt/ethicall/ethicall_frontend
+Mfile=/opt/ethicall/web_html5/`date +%Y%m%d-%H%M%S`
+Dfile=/mnt/document/odoo_document_dev/ethicall/ethicall_web_html5/`date +%Y%m%d-%H%M%S`
+Lockfile=/tmp/changename.lock
+
+
+if [ -e $Lockfile  ]
+then
+exit
+elif [ -d $Sfile ]
+then
+mv $Sfile $Mfile
+ln -s $Mfile $Dfile
+chown -R openerp.openerp $Dfile
+touch $Lockfile
+fi
