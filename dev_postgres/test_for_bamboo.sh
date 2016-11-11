814882a5bc215e1a82df07c785618410389b53c5
jun zhang <327101303@qq.com>
2016-11-11 20:12:05 +0800

    test

--- dev_postgres/test_for_bamboo.sh
+++ dev_postgres/test_for_bamboo.sh
@@ -3,5 +3,5 @@ echo $bamboo_build_working_directory
 echo $today
 time=`date +%Y%m%d`
 echo $time
-docker tag 8409f73da134 hub.local.ethicall.cn/postgres/pro_postgres:v1`$time`
-docker tag 8409f73da134 hub.local.ethicall.cn/postgres/pro_postgres:v`$today`
+docker tag 8409f73da134 hub.local.ethicall.cn/postgres/pro_postgres:v1$time
+docker tag 8409f73da134 hub.local.ethicall.cn/postgres/pro_postgres:v$today
