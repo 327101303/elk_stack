fb82526610118b6e0f96be59566b57a7e5a8a189
jun zhang <327101303@qq.com>
2016-11-13 20:22:40 +0800

    修改脚本名字

---
+++ odoo-to-docker/7/change_docker_image_name.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#New_Tag=`docker images|grep v$bamboo_buildNumber|awk -F ' ' '{print $3}'`
+docker tag hub.local.ethicall.cn/ethicall/ethicall:v$bamboo_buildNumber registry.cn-hangzhou.aliyuncs.com/zhangjun/ethicall:v$bamboo_buildNumber
